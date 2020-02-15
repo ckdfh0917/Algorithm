4aaf85c08cad7610f98a5709d2718c6ac7f441a0
ckdfh0917 <45144962+ckdfh0917@users.noreply.github.com>
2020-02-15 21:12:31 +0900

    home commit

--- SW-Expert-Academy/D3/3282. Knapsack.py
+++ SW-Expert-Academy/D3/3282. Knapsack.py
@@ -5,28 +5,28 @@ q = int(input())
 
 def getSubset(lst,K):
     n = len(lst)
-    result = []
+    max_C = 0
     for i in range(1<<n):
         res = []
         for j in range(n):
             temp = i & (1<<j)
             if temp:
                 res.append(lst[j])
-        sum_V = 0
-        sum_C = 0
         if res:
             flag = 0
-            # print('r',res)
-            # print('l',len(res))
+            sum_V = 0
+            sum_C = 0
             for k in range(len(res)):
-                sum_V += res[k][0]
-                sum_C += res[k][1]
+                sum_V += res[k][0]      # 부피
+                sum_C += res[k][1]      # 가치
                 if sum_V > K:
                     flag = 1
                     break
             if flag == 0:
-                result.append(sum_C)
-    return result
+                print(res)
+                if max_C < sum_C:
+                    max_C = sum_C
+    return max_C
 
 for test_case in range(1,q+1):
     N, K = map(int, input().split())
@@ -34,13 +34,8 @@ for test_case in range(1,q+1):
     box = []
     for _ in range(N):
         box.append(list(map(int, input().split())))
-    # print(box)
-    # print(box[0][0])
-    a = []
-    V = []
 
-    arr = getSubset(box,K)
-    # print('qqqq',arr)
+    res = getSubset(box,K)
+    #print('qqqq',res)
 
-    max_t = max(arr)
-    print('#{} {}' .format(test_case, max_t))
+    print('#{} {}' .format(test_case, res))
--- SW-Expert-Academy/D3/test.py
+++ SW-Expert-Academy/D3/test.py
@@ -6,37 +6,39 @@ q = int(input())
 def getSubset(lst):
     n = len(lst)
     result = []
-    a = []
-    maxT = 0
     for i in range(1<<n):
         res = []
         for j in range(n):
             temp = i & (1<<j)
             if temp:
                 res.append(lst[j])
-        #print('r',res)
-        maxTT = 0
-        if res:
-            sumT = 0
-            sumK = 0
-            for k in range(len(res)):
-                sumT += res[k][0]
-                sumK += res[k][1]
-                if sumK > L:
-                    break
-                if sumT > maxT:
-                    maxT = sumT
-    return maxT
+        result.append(res)
+    return result
 
-for test_case in range(1, q+1):
-    N, L = map(int, input().split())    # N: 재료수, L: 제한 칼로리
+for test_case in range(1,q+1):
+    N, K = map(int, input().split())
 
-    menu = []
-    for i in range(N):
-        temp = list(map(int, input().split()))
-        if temp[1] <= L:
-            menu.append(temp)
+    box = []
+    for _ in range(N):
+        box.append(list(map(int, input().split())))
 
-    maxTT = getSubset(menu)
+    arr = getSubset(box)
+    max_C = 0
+    for i in range(1,len(arr)):
+        sum_V = 0
+        sum_C = 0
+        flag = 0
+        for j in range(len(arr[i])):
+            sum_V += arr[i][j][0]
+            sum_C += arr[i][j][1]
+            if sum_V > K:
+                flag = 1
+                break
+        if flag == 0:
+            if max_C < sum_C:
+                print(arr[i])
+                max_C = sum_C
 
-    print('#{} {}'.format(test_case, maxTT))
+    #print('qqqq',res)
+
+    print('#{} {}' .format(test_case, max_C))
