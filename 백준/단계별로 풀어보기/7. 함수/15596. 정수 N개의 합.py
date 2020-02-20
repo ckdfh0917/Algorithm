03ed384ecb6f445ec5754e77c586e5ddce6e035c
ckdfh0917 <pmiwha1004@nate.com>
2020-02-20 18:08:25 +0900

    Add commit

---
+++ 백준/단계별로 풀어보기/7. 함수/15596. 정수 N개의 합.py
@@ -0,0 +1,7 @@
+def solve(a):
+    n = len(a)
+    res = 0
+    for i in range(n):
+        res += a[i]
+
+    return res
--- 백준/단계별로 풀어보기/7. 함수/4673. 셀프 넘버.py
+++ 백준/단계별로 풀어보기/7. 함수/4673. 셀프 넘버.py
@@ -1,33 +1,40 @@
-def SelfNumber(n):
-    if n > 10000:
-        return
-    elif 1 <= n < 10:
-        pass
-
 def Create(n):
     global arr
-    if n > 10:
+    if n > 10000:
         return
     elif n >= 1000:
+        a = n // 1000
+        b = n % 1000
+        c = b // 100
+        d = b % 100
+        e = d // 10
+        f = d % 10
+        g = n + a + c + e + f
+        arr[g] = 1
+    elif n >= 100:
         a = n // 100
         b = n % 100
         c = b // 10
         d = b % 10
-        e = a + c + d
+        e = n + a + c + d
         arr[e] = 1
-        Create(e)
     elif n >= 10:
         a = n // 10
         b = n % 10
         c = n + a + b
+        # print(n, c)
         arr[c] = 1
-        Create(c)
     else:
         a = n + n
-        arr[n] = 1
-        Create(a)
+        # print(n, a)
+        arr[a] = 1
 
-arr = [0] * 1001
+arr = [1] + [0] * 11000
 
-Create(1)
-print(arr)
+for i in range(1,10000):
+    Create(i)
+# print(arr)
+
+for i in range(1,10001):
+    if arr[i] == 0:
+        print(i)
