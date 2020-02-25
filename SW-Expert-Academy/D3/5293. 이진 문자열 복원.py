383972591821f0c3506bcf1e22a91e612c20065a
ckdfh0917 <45144962+ckdfh0917@users.noreply.github.com>
2020-02-25 18:07:28 +0900

    귱 commit

--- SW-Expert-Academy/D3/5293. 이진 문자열 복원.py
+++ SW-Expert-Academy/D3/5293. 이진 문자열 복원.py
@@ -2,30 +2,38 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 q = int(input())
-# q = 1
-dp = [[[[[False] * 2 for _ in range(20)] for _ in range(20)] for _ in range(20)] for _ in range(20)]
+#
+dp = [[[[[True] * 21 for _ in range(21)] for _ in range(21)] for _ in range(21)] for _ in range(2)]
+# dp[0][a][b][c][d]: 이전 0 이고, 00 a개 01 b개 10 c개 11 d개 있을 때 방문해봤으면 false
+# dp[1][a][b][c][d]: 이전 1 이고, 00 a개 01 b개 10 c개 11 d개 있을 때 방문해봤으면 false
 
-def find(k,a,b,c,d,str):
-    print(result)
-    if a == 0 and b == 0 and c == 0 and d == 0:
+
+def find(a,b,c,d,str):
+    # print(str)
+    past = int(str[-1])
+    # print(past)
+    if a == 0 and b == 0 and c == 0 and d == 0:     # a, b, c, d를 조건에 맞게 다 쓰면
+        print(str)
         return True
-    if dp[k][a][b][c][d] == False:
+    if dp[past][a][b][c][d] == False:       # DP -> 이미 실패한 곳은 다시 가지 않는다다
         return False
-    if k == 0:      # 마지막 자리가 0 이면
-        if a > 0:   # 00 이면
-            if find('0',a-1,b,c,d,result+'0'):
+
+    if past == 0:               # 마지막 자리가 0 이면
+        if a > 0:               # 00 이면
+            if find(a-1,b,c,d,str+'0'):
                 return True
-        if b > 0:   # 01 이면
-            if find('1',a,b-1,c,d,result+'1'):
+        if b > 0:               # 01 이면
+            if find(a,b-1,c,d,str+'1'):
                 return True
-    else:           # 마지막 자리가 1 이면
-        if c > 0:   # 10 이면
-            if find('0',a,b,c-1,d,result+'0'):
+    else:                       # 마지막 자리가 1 이면
+        if c > 0:               # 10 이면
+            if find(a,b,c-1,d,str+'0'):
                 return True
-        if d > 0:   # 11 이면
-            if find('1',a,b,c,d-1,result+'1'):
+        if d > 0:               # 11 이면
+            if find(a,b,c,d-1,str+'1'):
                 return True
-    dp[k][a][b][c][d] = False
+    # 재귀를 돌다가 조건에 맞는 abcd가 없다면 False
+    dp[past][a][b][c][d] = False
 
     return False
 
@@ -34,9 +42,8 @@ for test_case in range(1,q+1):
     A,B,C,D = map(int, input().split())
     print('#{} '.format(test_case), end='')
     result = ''
-    if find(0,A,B,C,D,result+'0'):
+    if find(A,B,C,D,'0'):
         continue
-    if find(1,A,B,C,D,result+'1'):
+    if find(A,B,C,D,'1'):
         continue
-    print(result)
     print('impossible')
