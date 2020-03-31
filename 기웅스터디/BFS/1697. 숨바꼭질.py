ce73cb5b1c2d219add5d7168128fbf581182c5b9
이창로 <pmiwha1004@nate.com>
2020-03-31 21:23:20 +0900

    0331

--- 기웅스터디/BFS/1697. 숨바꼭질.py
+++ 기웅스터디/BFS/1697. 숨바꼭질.py
@@ -1,40 +1,29 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-a, b = map(int, input().split())
-q = deque([])
+n, k = map(int, input().split())
+q = []
 visited = [0] * 100001
 
-def f(x, y):
-    # print(x, y)
-    res = 100000
-    if x > y:
-        return print(x-y)
-    elif x == y:
-        return print(0)
-    else:
-        q.append([y, 0])
-        visited[y] = 1
-        while q:
-            print(q)
-            t, cnt = q.popleft()
-            visited[t] = 1
-            print(t, cnt)
-            if t == x:
-                res = min(res, cnt)
-                print(res)
-                break
-            else:
-                if t % 2 == 0:
-                    if visited[t//2] == 0:
-                        q.append([t//2, cnt+1])
-                else:
-                    if visited[t-1] == 0:
-                        q.append([t-1, cnt+1])
-                    if visited[t+1] == 0:
-                        q.append([t+1, cnt+1])
-    if res == 100000:
-        pass
-    print(res)
-f(a, b)
+def bfs(n, k):
+    q.append([n, 0])
+    visited[n] = 1
 
+    while q:
+        # print(q)
+        a, b = q.pop(0)
+
+        if a == k:
+            return b
+
+        if a + 1 < 100001 and visited[a+1] == 0:
+            q.append([a+1, b+1])
+            visited[a+1] = 1
+        if a - 1 >= 0 and visited[a-1] == 0:
+            q.append([a-1, b+1])
+            visited[a-1] = 1
+        if a * 2 < 100001 and visited[a*2] == 0:
+            q.append([a*2, b+1])
+            visited[a*2] = 1
+
+
+print(bfs(n, k))
