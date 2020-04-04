65e81fea694c729ff57dc37f90cfc60e13d3a3b2
이창로 <pmiwha1004@nate.com>
2020-04-04 16:00:37 +0900

    0404

--- SW-Expert-Academy/D3/1244. 최대 상금.py
+++ SW-Expert-Academy/D3/1244. 최대 상금.py
@@ -1,10 +1,29 @@
 q = int(input())
 
+def cal(cnt, now):
+    global ans
+    if cnt == k:
+        a = int(''.join(map(str, s)))
+        ans = max(ans, a)
+        return
+
+    for i in range(now, len(s)):
+        for j in range(i, len(s)):
+            if i == j:
+                continue
+            if s[i] <= s[j]:
+                s[i], s[j] = s[j], s[i]
+                cal(cnt+1, i)
+                s[i], s[j] = s[j], s[i]
+
+
 for test_case in range(1,q+1):
-    n, k = map(str, input().split())
-    k = int(k)
-    print('#{} '.format(test_case), end='')
+    z = input().split()
+    n = z[0]
+    k = int(z[1])
 
-
-
-    
+    n = list(map(str, n))
+    ans = 0
+    s = n[:]
+    cal(0, 0)
+    print('#{} {}' .format(test_case, ans))
