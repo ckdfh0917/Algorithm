fd6feebc4e1a388b7a26d3c831b6efd745b04ca3
이창로 <pmiwha1004@nate.com>
2020-04-06 23:10:08 +0900

    0406

---
+++ 알고리즘/D15_QUEUE_01/이창로_보물섬.py
@@ -0,0 +1,6 @@
+m, n = map(int, input().split())
+
+arr = [input() for _ in range(m)]
+
+print(arr)
+print(arr[0][0])
--- 알쓸신잡/sort.py
+++ 알쓸신잡/sort.py
@@ -1,5 +1,6 @@
 # 내림차순
-a = [1, 10, 5, 7, 6]
+a = [[1, 10], [5, 7], [6, 10]]
 a.sort(reverse=True)
 print(a)
 
+b = sorted(a, lambda x:(x[1], x[0]))
