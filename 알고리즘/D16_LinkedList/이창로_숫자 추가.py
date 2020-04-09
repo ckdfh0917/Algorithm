d8c18e312fc1950a0dc3630c8786867d14ab791f
이창로 <pmiwha1004@nate.com>
2020-04-09 12:07:47 +0900

    0409

--- 알고리즘/D16_LinkedList/이창로_숫자 추가.py
+++
@@ -1,8 +0,0 @@
-T = int(input())
-for test_case in range(1, T+1):
-    N, M, L = map(int, input().split())
-    numbers = list(map(int, input().split()))
-    for _ in range(M):
-        idx, k = map(int, input().split())
-        numbers.insert(idx, k)
-    print('#{} {}' .format(test_case, numbers[L]))
