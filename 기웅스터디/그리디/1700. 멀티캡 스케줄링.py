1078859f26ccb95764ef8aba512cbfdd6e856312
이창로 <pmiwha1004@nate.com>
2020-04-26 21:56:07 +0900

    0426

--- 기웅스터디/그리디/1700. 멀티캡 스케줄링.py
+++ 기웅스터디/그리디/1700. 멀티캡 스케줄링.py
@@ -3,21 +3,30 @@ N, K = map(int, input().split())
 num = list(map(int, input().split()))
 stack = []
 visited = [0] * (K+1)
+cnt = 0
 for i in range(K):
+    # print(stack, num[i], visited, cnt)
     if len(stack) < N:
         stack.append(num[i])
+        # visited[num[i]] = 1
     else:
+        if num[i] in stack:
+            continue
         flag = 0
         for j in range(N):
             if i+j < K:
+                # print('aa',i,j,i+j)
                 if num[i+j] in stack:
                     visited[num[i+j]] = 1
         for j in range(len(stack)):
-            if stack[j] not in visited:
+            if visited[stack[j]] == 0:
                 stack[j] = num[i]
+                cnt += 1
                 flag = 1
                 break
 
-        if flag == 0:
-            for x in range(i+1, K):
-                visited[num[x]] =
+        # if flag == 0:
+        #     for x in range(i+1, K):
+        #         visited[num[x]] =
+
+print(cnt)
