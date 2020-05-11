6e50ea67d3dace32f0acc39e7be59615ae9fc168
이창로 <pmiwha1004@nate.com>
2020-05-11 20:05:24 +0900

    0511

--- 기웅스터디/Brute Force/2503. 숫자 야구.py
+++ 기웅스터디/Brute Force/2503. 숫자 야구.py
@@ -1,7 +1,41 @@
 N = int(input())
 game = []
+
+res = 0
 for _ in range(N):
-    k = map(int, input().split())
-    game.append([k])
+    numbers, s, b = map(int, input().split())
+    game.append([numbers,s,b])
+
+    num_1 = numbers[0]
+    num_2 = numbers[1]
+    num_3 = numbers[2]
+
+
+    for i in range(1, 10):
+        for j in range(1, 10):
+            for k in range(1, 10):
+                cnt_s = 0
+                cnt_b = 0
+                
+                if num_1 == i:
+                    cnt_s += 1
+                elif num_1 == j or num_1 == k:
+                    cnt_b += 1
+
+                if num_2 == i:
+                    cnt_s += 1
+                elif num_2 == j or num_2 == k:
+                    cnt_b += 1
+
+                if num_3 == i:
+                    cnt_s += 1
+                elif num_3 == j or num_3 == k:
+                    cnt_b += 1
+
+                if s == cnt_s and b == cnt_b:
+                    res += 1
 
 print(game)
+print(game[0])
+print(game[0][0])
+print(game[0][0][0])
