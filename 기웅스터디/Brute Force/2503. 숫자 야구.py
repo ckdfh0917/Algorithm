74588eafc9a2ba9043648aa4a672f3959a8c802d
이창로 <pmiwha1004@nate.com>
2020-05-12 01:55:06 +0900

    0512

--- 기웅스터디/Brute Force/2503. 숫자 야구.py
+++ 기웅스터디/Brute Force/2503. 숫자 야구.py
@@ -1,41 +1,53 @@
 N = int(input())
 game = []
 
-res = 0
 for _ in range(N):
     numbers, s, b = map(int, input().split())
     game.append([numbers,s,b])
 
-    num_1 = numbers[0]
-    num_2 = numbers[1]
-    num_3 = numbers[2]
+res = 0
 
+for i in range(1, 10):
+    for j in range(1, 10):
+        for k in range(1, 10):
+            temp = 0
+            if i == j or j == k or k == i:
+                continue
+            for x in range(N):
+                nums = game[x]
+                n = nums[0]
+                s = nums[1]
+                b = nums[2]
 
-    for i in range(1, 10):
-        for j in range(1, 10):
-            for k in range(1, 10):
+                num_1 = n // 100
+                num_2 = (n // 10) % 10
+                num_3 = n % 10
+
                 cnt_s = 0
                 cnt_b = 0
-                
+
                 if num_1 == i:
                     cnt_s += 1
                 elif num_1 == j or num_1 == k:
                     cnt_b += 1
 
-                if num_2 == i:
+                if num_2 == j:
                     cnt_s += 1
-                elif num_2 == j or num_2 == k:
+                elif num_2 == i or num_2 == k:
                     cnt_b += 1
 
-                if num_3 == i:
+                if num_3 == k:
                     cnt_s += 1
-                elif num_3 == j or num_3 == k:
+                elif num_3 == i or num_3 == j:
                     cnt_b += 1
+
 
                 if s == cnt_s and b == cnt_b:
-                    res += 1
+                    temp += 1
+                else:
+                    break
+            # print(temp)
+            if temp == N:
+                res += 1
 
-print(game)
-print(game[0])
-print(game[0][0])
-print(game[0][0][0])
+print(res)
---
+++ 기웅스터디/Brute Force/체스판 다시 칠하기.py
@@ -0,0 +1,21 @@
+N, M = map(int, input().split())
+
+chess = [input() for _ in range(N)]
+
+for i in range(N):
+    print(chess[i])
+
+new_chess =[]
+
+print(chess)
+
+for i in range(0,N-8+1):
+    for j in range(0,M-8+1):
+        print('ii', i, j)
+        print('cc', chess[i][j:j+8])
+        for k in range(8):
+            new_chess.append(chess[i+k][j:j+8])
+        print('nn', new_chess)
+
+        for k in range(8):
+            print('99', new_chess[k])
