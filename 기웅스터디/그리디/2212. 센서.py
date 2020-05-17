e7d9c4fb9c1770a83256ccb9b80b01f33f5f0db6
이창로 <pmiwha1004@nate.com>
2020-05-17 12:13:26 +0900

    0517

--- 기웅스터디/그리디/2212. 센서.py
+++ 기웅스터디/그리디/2212. 센서.py
@@ -1,9 +1,29 @@
 N = int(input())
 K = int(input())
 
-sensor = list(map(int, input().split()))
+sensor = sorted(list(map(int, input().split())))
 
-sum_s = sum(sensor)
-p = sum_s // N
-for i in range(N):
-    sdsd
+print(sensor)
+visited = [0] * max(sensor)
+# visited[0] = 1
+# visited[-1] = 1
+
+def find(k, v):
+    global ans
+    if k == 0:
+        print(v)
+        for i in range(len(v)):
+            if v[i] == 1:
+                pass
+        return
+    else:
+        for i in range(min(sensor)+1, max(sensor)):
+            print('aaa', i)
+            if v[i] == 0 and i in sensor:
+                print('bbb', i)
+                v[i-1] = 1
+                find(k-1, v)
+                v[i-1] = 0
+
+ans = 1234567891
+find(K*2 - 2, visited)
