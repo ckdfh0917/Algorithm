d9581eef5da0ceb8d7f810423daa6feaccff8c38
이창로 <pmiwha1004@nate.com>
2020-05-21 09:52:46 +0900

    0521

--- 기웅스터디/그리디/2212. 센서.py
+++ 기웅스터디/그리디/2212. 센서.py
@@ -4,26 +4,38 @@ K = int(input())
 sensor = sorted(list(map(int, input().split())))
 
 print(sensor)
-visited = [0] * max(sensor)
-# visited[0] = 1
-# visited[-1] = 1
+visited = [0] * (max(sensor) + 1)
+visited[min(sensor)] = 1
+visited[-1] = 1
 
 def find(k, v):
     global ans
     if k == 0:
-        print(v)
-        for i in range(len(v)):
-            if v[i] == 1:
-                pass
+        print('a', v)
+        arr = v[:]
+        cnt = 0
+        s = 0
+        for i in range(min(sensor), len(arr)):
+            if arr[i] == 1:
+                cnt += 1
+            if cnt % 2 == 1 and s == 0:
+                s = i
+                print('s', s)
+            elif cnt % 2 == 0 and cnt != 0:
+                e = i
+                print(s, e)
+                for j in range(s, e+1):
+                    arr[j] = 1
+                cnt = 0
+                s = 0
+        print('b', arr)
         return
     else:
         for i in range(min(sensor)+1, max(sensor)):
-            print('aaa', i)
             if v[i] == 0 and i in sensor:
-                print('bbb', i)
-                v[i-1] = 1
+                v[i] = 1
                 find(k-1, v)
-                v[i-1] = 0
+                v[i] = 0
 
 ans = 1234567891
 find(K*2 - 2, visited)
