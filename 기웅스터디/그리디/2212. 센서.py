d0979a94c254b1275f0ab918b9f622fe86ea96c7
이창로 <pmiwha1004@nate.com>
2020-05-27 22:53:32 +0900

    그리디

--- 기웅스터디/그리디/2212. 센서.py
+++ 기웅스터디/그리디/2212. 센서.py
@@ -5,44 +5,16 @@ lst = list(map(int, input().split()))
 lst.sort()
 lst = list(set(lst))
 res = []
+tmp = []
 minV = 1234567891
 visited = [0] * len(lst)
 visited[-1] = 1
+print(lst)
 def find(v, idx, ans):
     global res, minV
-    print(v, idx, ans)
+    print('a', v, idx, ans)
     if v.count(1) == K:
-        # print('v', v)
-        cnt = 0
-        idx = 0
-        for i in range(len(v)):
-            if cnt == K-1:
-                res.append([lst[i], lst[-1]])
-                break
-            if v[i] == 1:
-                if not res:
-                    res.append([lst[0], lst[i]])
-                    cnt += 1
-                else:
-                    res.append([lst[idx+1], lst[i]])
-                    cnt += 1
-                idx = i
 
-        if len(res) == K:
-            temp = 0
-            flag = False
-            for i in range(K):
-                s, e = res[i]
-                temp += e - s
-                # if s == e:
-                #     flag = True
-            if not flag:
-                minV = min(minV, temp)
-                print(res)
-                print(ans, temp)
-                print(minV)
-                print()
-        res = []
         return
     elif ans > minV:
         print('back')
@@ -51,20 +23,15 @@ def find(v, idx, ans):
         index = 0
         for i in range(idx, len(v)):
             # print('i', i)
-            if visited[i] == 0:
-                visited[i] = 1
-                print(visited)
-                if visited.count(1) == 1:
-                    # print('ans0', lst[i] - lst[0])
-                    find(v, i + 1, ans + lst[i] - lst[0])
-                else:
-                    # print('ans1', lst[i] - lst[index+1])
-                    if visited.count(1) == K:
-                        find(v, i + 1, ans + lst[i] - lst[index+1] + lst[-1] - lst[i+1] )
-                    else:
-                        find(v, i + 1, ans + lst[i] - lst[index+1])
-                visited[i] = 0
+            if v[i] == 0:
+                v[i] = 1
+                print(i, index, lst[i], lst[index])
+                find(v, i + 1, ans + lst[i] - lst[index])
+                v[i] = 0
                 index = i
 find(visited, 0, 0)
-# print('==============')
+print('==============')
 print(minV)
+
+
+
