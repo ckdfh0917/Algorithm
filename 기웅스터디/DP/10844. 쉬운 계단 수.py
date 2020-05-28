53c1c21e0b2beed0d12a78e010b02568d4a549bd
이창로 <pmiwha1004@nate.com>
2020-05-28 16:13:38 +0900

    DP

--- 기웅스터디/DP/10844. 쉬운 계단 수.py
+++ 기웅스터디/DP/10844. 쉬운 계단 수.py
@@ -1,2 +1,17 @@
 N = int(input())
 
+dp = [0] + [1] * 9
+# print(dp)
+
+for _ in range(N-1):
+    temp = [0] * 10
+    for i in range(10):
+        if i == 0:
+            temp[i] = dp[i+1]
+        elif i == 9:
+            temp[i] = dp[i-1]
+        else:
+            temp[i] = dp[i+1] + dp[i-1]
+    dp = temp[:]
+    # print(dp)
+print(sum(dp) % 1000000000)
