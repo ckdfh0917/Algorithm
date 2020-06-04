1ee4d989bbc58e135f2ea0a4476612892a8f65db
이창로 <pmiwha1004@nate.com>
2020-06-05 01:32:57 +0900

    union find

--- 백준/10775. 공항.py
+++ 백준/10775. 공항.py
@@ -1,11 +1,38 @@
 G = int(input())
 P = int(input())
+
+def make_set(x):
+    p[x] = x
+
+def find_set(x):
+    if x == p[x]:
+        return x
+    else:
+        p[x] = find_set(p[x])
+        return p[x]
+
+def union(x, y):
+    py = p[find_set(y)]
+    px = p[find_set(x)]
+    p[py] = px
+
+
 gate = []
-checked = [0] * (G+1)
+p = [0] * (G+1)
+
 for i in range(P):
     gate.append(int(input()))
 
-print(gate)
+for i in range(G+1):
+    make_set(i)
 
+cnt = 0
+for i in range(P):
+    idx = find_set(gate[i])
+    if idx != 0:
+        union(idx-1, idx)
+        cnt += 1
+    else:
+        break
 
-
+print(cnt)
