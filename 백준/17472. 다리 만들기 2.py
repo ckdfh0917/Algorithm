51944cfbc09dae37f82db6389a53c138d8794acf
이창로 <pmiwha1004@nate.com>
2020-06-09 23:48:41 +0900

    union_find dfs

--- 백준/17472. 다리 만들기 2.py
+++ 백준/17472. 다리 만들기 2.py
@@ -1,3 +1,4 @@
+import heapq
 N, M = map(int, input().split())
 
 land = [list(map(int, input().split())) for _ in range(N)]
@@ -17,6 +18,69 @@ def marking(x, y ,cnt):
                 marking(x+dx[i], y+dy[i], cnt)
     return
 
+def row_cnt():
+    for i in range(N):
+        d = 0
+        temp = 0
+        for j in range(M):
+            # print(i, j, end=' ')
+            if temp == 0 and land[i][j] == 0:
+                # print('a')
+                continue
+            elif temp == 0 and land[i][j] != 0:
+                temp = land[i][j]
+                # print('b')
+            elif temp != 0 and land[i][j] == 0:
+                d += 1
+                # print('c', d)
+            elif land[i][j] != temp:
+                heapq.heappush(pq, (d, land[i][j], temp))
+                temp = land[i][j]
+                # print('d', pq)
+                d = 0
+            elif land[i][j] == temp:
+                # print('e')
+                continue
+
+def col_cnt():
+    for j in range(M):
+        d = 0
+        temp = 0
+        for i in range(N):
+            # print(i, j, end=' ')
+            if temp == 0 and land[i][j] == 0:
+                # print('a')
+                continue
+            elif temp == 0 and land[i][j] != 0:
+                temp = land[i][j]
+                # print('b')
+            elif temp != 0 and land[i][j] == 0:
+                d += 1
+                # print('c', d)
+            elif land[i][j] != temp:
+                heapq.heappush(pq, (d, land[i][j], temp))
+                temp = land[i][j]
+                # print('d', pq)
+                d = 0
+            elif land[i][j] == temp:
+                # print('e')
+                continue
+
+def make_set(x):
+    pi[x] = x
+
+def find_set(x):
+    if pi[x] == x:
+        return x
+    else:
+        pi[x] = find_set(pi[x])
+        return pi[x]
+
+def union(x, y):
+    py = pi[find_set(y)]
+    px = pi[find_set(x)]
+    pi[py] = px
+
 cnt = 0
 visited = [[0] * M for _ in range(N)]
 
@@ -27,9 +91,41 @@ for i in range(N):
                 cnt += 1
                 marking(i, j, cnt)
 
-for i in range(N):
-    print(land[i])
+# for i in range(N):
+#     print(land[i])
 
-pq = [0] * (cnt+1)
-print(pq)
+pq = []
 
+row_cnt()
+col_cnt()
+
+# print(pq)
+
+
+pi = [0] * (cnt+1)
+
+for i in range(1, cnt):
+    make_set(i)
+
+# print(pq)
+n = 0
+ans = 0
+while n < cnt-1:
+    # print(pq)
+    if not pq:
+        ans = -1
+        break
+    d, a, b = heapq.heappop(pq)
+    # print(d, a, b)
+    # print()
+    if d < 2:
+        continue
+
+    if find_set(a) == find_set(b):
+        continue
+    else:
+        union(a, b)
+        ans += d
+        n += 1
+
+print(ans)
