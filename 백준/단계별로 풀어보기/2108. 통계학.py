56c069b7d976a82a283e140c712e3eb50ba55819
이창로 <pmiwha1004@nate.com>
2020-07-19 14:11:34 +0900

    정렬

--- 백준/단계별로 풀어보기/2108. 통계학.py
+++ 백준/단계별로 풀어보기/2108. 통계학.py
@@ -5,12 +5,30 @@ for _ in range(N):
     numbers.append(int(input()))
 
 res1 = sum(numbers) / N
-res2 = {}
-res3 = 0
+res2 = sorted(numbers)
+res3 = {}
 res4 = max(numbers) - min(numbers)
 
+print(round(res1))
+print(res2[N//2])
+
+new_num = [0] * 9000
 
 for i in range(N):
-    res2[numbers[i]] = numbers.count(numbers[i])
+    new_num[numbers[i]+4000] += 1
 
-print(res2)
+maxV = max(new_num)
+
+if new_num.count(maxV) == 1:
+    print(new_num.index(maxV)-4000)
+else:
+    flag = 0
+    for i in range(len(new_num)):
+        if new_num[i] == maxV:
+            if flag == 0:
+                flag += 1
+            else:
+                print(i - 4000)
+                break
+
+print(res4)
