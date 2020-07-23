d2bf82b35a11db2264d672f19690fed37bc94143
이창로 <pmiwha1004@nate.com>
2020-07-23 21:20:00 +0900

    DP

---
+++ 백준/단계별로 풀어보기/1149. RGB거리.py
@@ -0,0 +1,3 @@
+N = int(input())
+for _ in range(N):
+    r, g, b = map(int, input().split())
---
+++ 백준/단계별로 풀어보기/9461. 파도반 수열.py
@@ -0,0 +1,15 @@
+T = int(input())
+
+dp = [0] * 101
+dp[0] = 1
+dp[1] = 1
+dp[2] = 1
+dp[3] = 2
+dp[4] = 2
+
+for i in range(5, 101):
+    dp[i] = dp[i-1] + dp[i-5]
+
+for _ in range(T):
+    N = int(input())
+    print(dp[N-1])
