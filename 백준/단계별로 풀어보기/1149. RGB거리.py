596933be6ccd1b85601e3b5b1c12908298ec2d5e
이창로 <pmiwha1004@nate.com>
2020-07-26 09:30:32 +0900

    dp

--- 백준/단계별로 풀어보기/1149. RGB거리.py
+++ 백준/단계별로 풀어보기/1149. RGB거리.py
@@ -1,8 +1,7 @@
 N = int(input())
 
-cnt = 12345678901
-home = [[0, 0, 0] for _ in range(1002)]
-dp = [[0, 0, 0] for _ in range(1002)]
+home = [[0, 0, 0] for _ in range(N+1)]
+dp = [[0, 0, 0] for _ in range(N+1)]
 
 for i in range(N):
     r, g, b = map(int, input().split())
@@ -15,14 +14,8 @@ dp[0][1] = home[0][1]
 dp[0][2] = home[0][2]
 
 for i in range(1, N):
-    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + home[i][0]
-    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + home[i][1]
-    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + home[i][2]
-    # print('d', dp)
-# print(dp)
+    dp[i][0] = home[i][0] + min(dp[i-1][1], dp[i-1][2])
+    dp[i][1] = home[i][1] + min(dp[i-1][2], dp[i-1][0])
+    dp[i][2] = home[i][2] + min(dp[i-1][0], dp[i-1][1])
 
-for i in range(3):
-    if dp[N-1][i] < cnt:
-        cnt = dp[N-1][i]
-
-print(cnt)
+print(min(dp[N-1]))
