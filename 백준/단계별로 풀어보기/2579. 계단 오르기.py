7c830e36e78cd4821be23679e69192ac24508097
이창로 <pmiwha1004@nate.com>
2020-08-12 00:08:09 +0900

    DP

--- 백준/단계별로 풀어보기/2579. 계단 오르기.py
+++ 백준/단계별로 풀어보기/2579. 계단 오르기.py
@@ -2,11 +2,14 @@ N = int(input())
 arr = []
 for i in range(N):
     arr.append(int(input()))
-print(arr)
 
-dp = [0] * N
-dp[0] = arr[0]
-dp[1] = arr[0] + arr[1]
+dp = [[0, 0] for _ in range(N)]
 
-for i in range(2, N):
-    dp[i] = max(dp[0] + arr[i], dp)
+dp[0] = [arr[0], arr[0]]
+if N > 1:
+    dp[1] = [arr[1], arr[0] + arr[1]]
+    for i in range(2, N):
+        dp[i][0] = max(dp[i-2]) + arr[i]
+        dp[i][1] = dp[i-1][0] + arr[i]
+
+print(max(dp[N-1]))
