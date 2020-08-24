f1c4cff2eb9e88c548f26e255066fb20b9967440
이창로 <pmiwha1004@nate.com>
2020-08-25 08:52:33 +0900

    bfs

--- 백준/삼성SW역량테스트기출문제/16234. 인구 이동.py
+++ 백준/삼성SW역량테스트기출문제/16234. 인구 이동.py
@@ -1,3 +1,4 @@
+from collections import deque
 N, L, R = map(int, input().split())
 
 arr = [list(map(int, input().split())) for _ in range(N)]
@@ -5,90 +6,42 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-p = [0] * N * N
-
-def make_set(x):
-    p[x] = x
-
-def find_set(x):
-    if x == p[x]:
-        return x
-    else:
-        p[x] = find_set(p[x])
-        return p[x]
-
-def union(x, y):
-    py = p[find_set(y)]
-    px = p[find_set(x)]
-    p[py] = px
-
-    # for i in range(N*N):
-    #     if py == find_set(i):
-    #         p[i] = px
-
 # 연합한 국가 인구수 더하기 및 연합 나라 숫자 세기
 def check():
     visited = [[0] * N for _ in range(N)]
-
-    c = 0
-    for i in range(N):
-        for j in range(N):
-            make_set(c)
-            c += 1
-
-    man = [0] * (N*N)
     flag = False
     for i in range(N):
         for j in range(N):
-            cnt = (N) * (i) + j
-            T = find_set(cnt)
-            # print('cnt', cnt, T)
-            for k in range(4):
-                # print('ii', i+dx[k], j+dy[k])
-                if 0 <= i+dx[k] < N and 0 <= j+dy[k] < N:
-                    if L <= abs(arr[i][j] - arr[i+dx[k]][j+dy[k]]) <= R:
-                        flag = True
-                        if k == 0:
-                            if T != find_set(cnt-N):
-                                print('c', cnt, cnt+1, find_set(cnt), find_set(cnt-N))
-                                union(cnt, cnt-N)
-                                print(k, p)
-                        elif k == 1:
-                            if T != find_set(cnt+1):
-                                print('c', cnt, cnt+1, find_set(cnt), find_set(cnt+1))
-                                union(cnt, cnt+1)
-                                print(k, p)
-                        elif k == 2:
-                            if T != find_set(cnt+N):
-                                print('c', cnt, cnt+1, find_set(cnt), find_set(cnt+N))
-                                union(cnt, cnt+N)
-                                print(k, p)
-                        elif k == 3:
-                            if T != find_set(cnt-1):
-                                print('c', cnt, cnt+1, find_set(cnt), find_set(cnt-
-                                                                               1))
-                                union(cnt, cnt-1)
-                                print(k, p)
+            if visited[i][j] == 1:
+                continue
+            cnt = 0
+            q = deque([[i, j]])
+            visited[i][j] = 1
+            sumV = 0
+            stack = [[i, j]]
+            # 연합할 수 있는 국가 찾기
+            #   - cnt : 연합한 국가 수
+            #   - sumV : 연합한 국가 사람들의 합
+            while q:
+                x, y = q.popleft()
+                cnt += 1
+                sumV += arr[x][y]
+                for k in range(4):
+                    if 0 <= x+dx[k] < N and 0 <= y+dy[k] < N:
+                        if visited[x+dx[k]][y+dy[k]] == 1:
+                            continue
+                        if L <= abs(arr[x][y] - arr[x+dx[k]][y+dy[k]]) <= R:
+                            flag = True
+                            q.append([x+dx[k], y+dy[k]])
+                            stack.append([x+dx[k], y+dy[k]])
+                            visited[x+dx[k]][y+dy[k]] = 1
+
+            # 연합한 국가들 1/N 적용
+            if cnt > 1:
+                for x, y in stack:
+                    arr[x][y] = sumV // cnt
 
     if flag:
-        print('ppp', p)
-        cnt = 0
-        for i in range(N):
-            for j in range(N):
-                man[p[cnt]] += arr[i][j]
-                cnt += 1
-
-        print('mmmm', man)
-        cnt = 0
-        for i in range(N):
-            for j in range(N):
-                if p.count(p[cnt]) > 1:
-                    temp = man[p[cnt]] // p.count(p[cnt])
-                    arr[i][j] = temp
-
-                cnt += 1
-        for i in range(N):
-            print(arr[i])
         return True
     else:
         return False
