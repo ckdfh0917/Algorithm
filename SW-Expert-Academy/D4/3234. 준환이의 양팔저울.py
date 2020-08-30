83af22589ffe4e0b141b0538499f8b14672f1eb9
이창로 <pmiwha1004@nate.com>
2020-08-30 22:32:58 +0900

    .

--- SW-Expert-Academy/D4/3234. 준환이의 양팔저울.py
+++ SW-Expert-Academy/D4/3234. 준환이의 양팔저울.py
@@ -2,58 +2,36 @@ T = int(input())
 
 def per(idx, new_left, new_right, new_visited):
     global ans
-    print(idx, new_left, new_right, new_visited)
-    if 0 not in new_visited:
-        flag = False
-        for i in range(N):
-            # print('aa', sum(new_left[:i+1]), 'bb', sum(new_right[:i+1]))
-            if sum(new_left[:i+1]) >= sum(new_right[:i+1]):
-                flag = True
-            else:
-                flag = False
-                break
-        if flag:
-            ans += 1
-            print('lllllll', new_left, 'rr', new_right)
-            return
-        else:
-            return
-
-    for i in range(N):
+    if idx == N:
+        # print(idx, new_left, new_right, new_visited)
+        ans += 1
+        return
+    # left 를 보는 것
+    for i in range(0, N):
+        # print('i', i, new_left, new_right, new_visited , idx)
         if new_visited[i] == 0:
             # print(new_left[idx])
             if left[i] != 0:
+                if new_left+left[i] < new_right:
+                    continue
                 new_visited[i] = 1
-                new_left[idx] = left[i]
-                per(idx+1, new_left, new_right, new_visited)
-                new_left[idx] = 0
+                per(idx+1, new_left + left[i], new_right, new_visited)
                 new_visited[i] = 0
 
             else:
+                if new_left < new_right + k[i]:
+                    continue
                 new_visited[i] = 1
-                new_right[idx] = right[i]
-                print('cc', idx, new_right, right)
-                per(idx+1, new_left, new_right, new_visited)
-                new_right[idx] = 0
+                per(idx+1, new_left, new_right + k[i], new_visited)
                 new_visited[i] = 0
 
 
 def dfs(idx):
     global ans
-    for i in range(N):
-        if visitied[i] == 0:
-            right[i] = k[i]
-        else:
-            right[i] = 0
-
-    if sum(left) >= sum(right):
-        print('qqq', left, right)
-        new_left = [0] * N
-        new_right = [0] * N
+    if sum(left) >= sum(k) - sum(left):
+        # print('qqq', left)
         new_visited = [0] * N
-        per(0, new_left, new_right, new_visited)
-        return
-
+        per(0, 0, 0, new_visited)
 
     for i in range(idx, N):
         if visitied[i] == 0:
@@ -70,7 +48,6 @@ for i in range(1, T+1):
     k = list(map(int, input().split()))
     visitied = [0] * N
     left = [0] * N
-    right = [0] * N
     ans = 0
     dfs(0)
     print('#{} {}' .format(i, ans))
--- SW-Expert-Academy/D4/test.py
+++ SW-Expert-Academy/D4/test.py
@@ -1,49 +1,48 @@
 T = int(input())
 
+dp = [0] * 11
+def fectorial(n):
+    if n == 1:
+        dp[1] = 1
+    elif n == 2:
+        dp[2] = 2
+    elif n == 0:
+        dp[0] = 1
+    else:
+        dp[n] = dp[n-1] * n
+    return
 
-def dfs(idx):
+for i in range(10):
+    fectorial(i)
+
+# print(dp)
+def dfs(idx, left, right):
     global ans
-    print(left, right)
-    if 0 not in visitied:
-        flag = False
-        print('ll', left, 'rr', right)
+    # print('left',idx,  left, right, visitied)
+
+    if left * 2 >= sum_k:
+        ans += 2 ** (N-idx) * dp[N-idx]
+        # print(ans, N-idx, fectorial(N-idx))
+        return
+    else:
         for i in range(N):
-            # print('aa', sum(left[:i+1]), 'bb', sum(right[:i+1]))
-            if sum(left[:i+1]) >= sum(right[:i+1]):
+            if visitied[i] == 0:
+                visitied[i] = 1
+                dfs(idx+1, left+k[i], right)
+                if left >= right+k[i]:
+                    dfs(idx+1, left, right+k[i])
+                visitied[i] = 0
 
-                flag = True
-            else:
-                flag = False
-                break
-        if flag:
-            ans += 1
-            # print('ll', left, 'rr', right)
-            return
-        else:
-            return
 
-    for i in range(N):
-        if visitied[i] == 0:
-            visitied[i] = 1
-            left[idx] = k[i]
-            dfs(idx+1)
-            left[idx] = 0
-            visitied[i] = 0
-
-            visitied[i] = 1
-            right[idx] = k[i]
-            dfs(idx+1)
-            right[idx] = 0
-            visitied[i] = 0
+    return
 
 
 
 for i in range(1, T+1):
     N = int(input())
     k = list(map(int, input().split()))
+    sum_k = sum(k)
     visitied = [0] * N
-    left = [0] * N
-    right = [0] * N
     ans = 0
-    dfs(0)
-    print('#{} {}'. format(i, ans))
+    dfs(0, 0, 0)
+    print('#{} {}' .format(i, ans))
