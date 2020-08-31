85210ebf8ab4631f6293a46eb0cb940daae8e755
이창로 <pmiwha1004@nate.com>
2020-08-31 18:26:49 +0900

    백트래킹

--- SW-Expert-Academy/D4/3234. 준환이의 양팔저울.py
+++ SW-Expert-Academy/D4/3234. 준환이의 양팔저울.py
@@ -1,53 +1,48 @@
 T = int(input())
 
-def per(idx, new_left, new_right, new_visited):
+dp = [0] * 11
+def fectorial(n):
+    if n == 1:
+        dp[1] = 1
+    elif n == 2:
+        dp[2] = 2
+    elif n == 0:
+        dp[0] = 1
+    else:
+        dp[n] = dp[n-1] * n
+    return
+
+for i in range(10):
+    fectorial(i)
+
+# print(dp)
+def dfs(idx, left, right):
     global ans
-    if idx == N:
-        # print(idx, new_left, new_right, new_visited)
-        ans += 1
+    # print('left',idx,  left, right, visitied)
+
+    if left * 2 >= sum_k:
+        ans += 2 ** (N-idx) * dp[N-idx]
+        # print(ans, N-idx, fectorial(N-idx))
         return
-    # left 를 보는 것
-    for i in range(0, N):
-        # print('i', i, new_left, new_right, new_visited , idx)
-        if new_visited[i] == 0:
-            # print(new_left[idx])
-            if left[i] != 0:
-                if new_left+left[i] < new_right:
-                    continue
-                new_visited[i] = 1
-                per(idx+1, new_left + left[i], new_right, new_visited)
-                new_visited[i] = 0
-
-            else:
-                if new_left < new_right + k[i]:
-                    continue
-                new_visited[i] = 1
-                per(idx+1, new_left, new_right + k[i], new_visited)
-                new_visited[i] = 0
+    else:
+        for i in range(N):
+            if visitied[i] == 0:
+                visitied[i] = 1
+                dfs(idx+1, left+k[i], right)
+                if left >= right+k[i]:
+                    dfs(idx+1, left, right+k[i])
+                visitied[i] = 0
 
 
-def dfs(idx):
-    global ans
-    if sum(left) >= sum(k) - sum(left):
-        # print('qqq', left)
-        new_visited = [0] * N
-        per(0, 0, 0, new_visited)
-
-    for i in range(idx, N):
-        if visitied[i] == 0:
-            visitied[i] = 1
-            left[i] = k[i]
-            dfs(i+1)
-            left[i] = 0
-            visitied[i] = 0
+    return
 
 
 
 for i in range(1, T+1):
     N = int(input())
     k = list(map(int, input().split()))
+    sum_k = sum(k)
     visitied = [0] * N
-    left = [0] * N
     ans = 0
-    dfs(0)
+    dfs(0, 0, 0)
     print('#{} {}' .format(i, ans))
