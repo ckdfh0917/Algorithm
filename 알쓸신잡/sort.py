f726608828feff3569db3107d81ae61b1642b654
이창로 <pmiwha1004@nate.com>
2020-09-05 13:48:02 +0900

    sort

--- 알쓸신잡/sort.py
+++ 알쓸신잡/sort.py
@@ -1,14 +1,14 @@
 # 내림차순
 a = [[1, 10], [1, 4], [5, 7], [6, 10]]
 a.sort(reverse=True)
-print(a)
+print(a)  # [[6, 10], [5, 7], [1, 10], [1, 4]]
 
 a.sort()
-print(a)
+print(a)  # [[1, 4], [1, 10], [5, 7], [6, 10]]
 
-a.sort(key=lambda x:(x[0], x[1]))
-print(a)
+a.sort(key=lambda x: (x[0], x[1]))
+print(a)  # [[1, 4], [1, 10], [5, 7], [6, 10]]
 
-b = sorted(a, key=lambda x:(x[1], x[0]))
-print(b)
+b = sorted(a, key=lambda x: (x[1], x[0]))
+print(b)  # [[1, 4], [5, 7], [1, 10], [6, 10]]
 
