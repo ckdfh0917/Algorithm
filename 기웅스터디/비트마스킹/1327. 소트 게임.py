03061856479a382e3ea9eb0e7d276dae818c1edf
ckdfh0917 <pmiwha1004@nate.com>
2020-10-15 13:17:56 +0900

    set

--- 기웅스터디/비트마스킹/1327. 소트 게임.py
+++ 기웅스터디/비트마스킹/1327. 소트 게임.py
@@ -1,5 +1,38 @@
+from collections import deque
+
 N, K = map(int, input().split())
-num = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
 
 
+visited = set()
+visited.add(str(numbers))
+
+# print(visited)
+q = deque()
+q.append([numbers, 0])
+
+ans = -1
+sorted_num = sorted(numbers)
+
+while q:
+    now, cnt = q.popleft()
+
+    if now == sorted_num:
+        ans = cnt
+        break
+
+    for i in range(N-K+1):
+        temp = now[:]
+        sub_num = now[i:i+K]
+        sub_num.reverse()
+
+        for j in range(K):
+            temp[i+j] = sub_num[j]
+
+        if str(temp) not in visited:
+            # print(str(temp))
+            visited.add(str(temp))
+            q.append([temp, cnt+1])
+
+print(ans)
 
