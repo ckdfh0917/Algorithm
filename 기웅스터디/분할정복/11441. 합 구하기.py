aa986c12767bec9095284ee947da8f232600a249
ckdfh0917 <pmiwha1004@nate.com>
2020-11-02 09:50:25 +0900

    prefix sum

--- 기웅스터디/분할정복/11441. 합 구하기.py
+++ 기웅스터디/분할정복/11441. 합 구하기.py
@@ -1,1 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+
+prefix_sum = [0] * (N+1)
+prefix_sum[1] = A[0]
+for i in range(1, N):
+    prefix_sum[i+1] = prefix_sum[i] + A[i]
+
+# print(prefix_sum)
+for _ in range(M):
+    i, j = map(int, input().split())
+    res = prefix_sum[j] - prefix_sum[i-1]
+    print(res)
