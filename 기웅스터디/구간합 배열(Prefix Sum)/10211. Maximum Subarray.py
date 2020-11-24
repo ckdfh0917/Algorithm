30edb1f5d8178570c78f7e70ad7088e044fdf7dc
이창로 <pmiwha1004@nate.com>
2020-11-24 14:16:20 +0900

    구간합

--- 기웅스터디/구간합 배열(Prefix Sum)/10211. Maximum Subarray.py
+++ 기웅스터디/구간합 배열(Prefix Sum)/10211. Maximum Subarray.py
@@ -1,2 +1,13 @@
 N = int(input())
-zzzz
+
+for _ in range(N):
+    k = int(input())
+    X = list(map(int, input().split()))
+    dp = [0] * k
+    dp[0] = X[0]
+    for i in range(1, k):
+        t1 = dp[i-1] + X[i]
+        t2 = X[i]
+        dp[i] = max(t1, t2)
+
+    print(max(dp))
