a835c790e83c480a1706817ee6b607416f30665f
이창로 <pmiwha1004@nate.com>
2020-11-26 12:25:03 +0900

    시뮬레이션

--- 백준/삼성SW역량테스트기출문제/test.py
+++ 백준/삼성SW역량테스트기출문제/test.py
@@ -22,14 +22,6 @@ def command(c):
         cube[4][0][2] = temp1
         cube[4][1][2] = temp2
         cube[4][2][2] = temp3
-
-        # 시계 회전
-        temp = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                temp[j][2-i] = cube[2][i][j]
-
-        cube[2] = temp
     elif c == 'R-':
         temp1 = cube[0][0][2]
         temp2 = cube[0][1][2]
@@ -50,14 +42,6 @@ def command(c):
         cube[1][0][2] = temp1
         cube[1][1][2] = temp2
         cube[1][2][2] = temp3
-
-        # 반시계 회전
-        temp = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                temp[2-j][i] = cube[2][i][j]
-
-        cube[2] = temp
     elif c == 'L+':
         temp1 = cube[0][0][0]
         temp2 = cube[0][1][0]
@@ -78,15 +62,6 @@ def command(c):
         cube[1][0][0] = temp1
         cube[1][1][0] = temp2
         cube[1][2][0] = temp3
-
-        # 시계 회전
-        temp = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                temp[j][2-i] = cube[3][i][j]
-
-        cube[3] = temp
-
     elif c == 'L-':
         temp1 = cube[0][0][0]
         temp2 = cube[0][1][0]
@@ -107,73 +82,95 @@ def command(c):
         cube[4][0][0] = temp1
         cube[4][1][0] = temp2
         cube[4][2][0] = temp3
-
-        # 반시계 회전
-        temp = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                temp[2-j][i] = cube[3][i][j]
-
-        cube[3] = temp
-
     elif c == 'U+':
-        temp1 = cube[4][2][0]
-        temp2 = cube[4][2][1]
-        temp3 = cube[4][2][2]
+        temp1 = cube[4][0][0]
+        temp2 = cube[4][0][1]
+        temp3 = cube[4][0][2]
+        temp4 = cube[4][1][0]
+        temp5 = cube[4][1][1]
+        temp6 = cube[4][1][2]
 
-        cube[4][2][0] = cube[3][2][2]
-        cube[4][2][1] = cube[3][1][2]
-        cube[4][2][2] = cube[3][0][2]
 
-        cube[3][2][2] = cube[1][0][2]
-        cube[3][1][2] = cube[1][0][1]
-        cube[3][0][2] = cube[1][0][0]
+        cube[4][0][0] = cube[2][0][2]
+        cube[4][0][1] = cube[2][1][2]
+        cube[4][0][2] = cube[2][2][2]
+        cube[4][1][0] = cube[2][0][1]
+        cube[4][1][1] = cube[2][1][1]
+        cube[4][1][2] = cube[2][2][1]
 
-        cube[1][0][2] = cube[2][0][0]
-        cube[1][0][1] = cube[2][1][0]
-        cube[1][0][0] = cube[2][2][0]
+        cube[2][0][2] = cube[1][2][2]
+        cube[2][1][2] = cube[1][2][1]
+        cube[2][2][2] = cube[1][2][0]
+        cube[2][0][1] = cube[1][1][2]
+        cube[2][1][1] = cube[1][1][1]
+        cube[2][2][1] = cube[1][1][0]
 
-        cube[2][0][0] = temp1
-        cube[2][1][0] = temp2
-        cube[2][2][0] = temp3
+        cube[1][2][2] = cube[3][2][0]
+        cube[1][2][1] = cube[3][1][0]
+        cube[1][2][0] = cube[3][0][0]
+        cube[1][1][2] = cube[3][2][1]
+        cube[1][1][1] = cube[3][1][1]
+        cube[1][1][0] = cube[3][0][1]
 
-        # 시계 회전
+        cube[3][2][0] = temp1
+        cube[3][1][0] = temp2
+        cube[3][0][0] = temp3
+        cube[3][2][1] = temp4
+        cube[3][1][1] = temp5
+        cube[3][0][1] = temp6
+
+
         temp = [[0] * 3 for _ in range(3)]
         for i in range(3):
             for j in range(3):
-                temp[j][2-i] = cube[0][i][j]
+                temp[j][2-i] = cube[5][i][j]
 
-        cube[0] = temp
-
-
+        cube[5] = temp
     elif c == 'U-':
-        temp1 = cube[4][2][0]
-        temp2 = cube[4][2][1]
-        temp3 = cube[4][2][2]
+        temp1 = cube[4][0][0]
+        temp2 = cube[4][0][1]
+        temp3 = cube[4][0][2]
+        temp4 = cube[4][1][0]
+        temp5 = cube[4][1][1]
+        temp6 = cube[4][1][2]
 
-        cube[4][2][0] = cube[2][0][0]
-        cube[4][2][1] = cube[2][1][0]
-        cube[4][2][2] = cube[2][2][0]
 
-        cube[2][0][0] = cube[1][0][2]
-        cube[2][1][0] = cube[1][0][1]
-        cube[2][2][0] = cube[1][0][0]
+        cube[4][0][0] = cube[3][2][0]
+        cube[4][0][1] = cube[3][1][0]
+        cube[4][0][2] = cube[3][0][0]
+        cube[4][1][0] = cube[3][2][1]
+        cube[4][1][1] = cube[3][1][1]
+        cube[4][1][2] = cube[3][0][1]
 
-        cube[1][0][2] = cube[3][2][2]
-        cube[1][0][1] = cube[3][1][2]
-        cube[1][0][0] = cube[3][0][2]
+        cube[3][2][0] = cube[1][2][2]
+        cube[3][1][0] = cube[1][2][1]
+        cube[3][0][0] = cube[1][2][0]
+        cube[3][2][1] = cube[1][1][2]
+        cube[3][1][1] = cube[1][1][1]
+        cube[3][0][1] = cube[1][1][0]
 
-        cube[3][2][2] = temp1
-        cube[3][1][2] = temp2
-        cube[3][0][2] = temp3
+        cube[1][2][2] = cube[2][0][2]
+        cube[1][2][1] = cube[2][1][2]
+        cube[1][2][0] = cube[2][2][2]
+        cube[1][1][2] = cube[2][0][1]
+        cube[1][1][1] = cube[2][1][1]
+        cube[1][1][0] = cube[2][2][1]
 
-        # 반시계 회전
+        cube[2][0][2] = temp1
+        cube[2][1][2] = temp2
+        cube[2][2][2] = temp3
+        cube[2][0][1] = temp4
+        cube[2][1][1] = temp5
+        cube[2][2][1] = temp6
+
         temp = [[0] * 3 for _ in range(3)]
+
         for i in range(3):
             for j in range(3):
-                temp[2-j][i] = cube[0][i][j]
+                temp[2-j][i] = cube[5][i][j]
 
-        cube[0] = temp
+        cube[5] = temp
+
     elif c == 'D+':
         temp1 = cube[4][0][0]
         temp2 = cube[4][0][1]
@@ -194,13 +191,6 @@ def command(c):
         cube[3][2][0] = temp1
         cube[3][1][0] = temp2
         cube[3][0][0] = temp3
-        # 시계 회전
-        temp = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                temp[j][2-i] = cube[5][i][j]
-
-        cube[5] = temp
 
     elif c == 'D-':
         temp1 = cube[4][0][0]
@@ -222,13 +212,7 @@ def command(c):
         cube[2][2][2] = temp3
         cube[2][1][2] = temp2
         cube[2][0][2] = temp1
-        # 반시계 회전
-        temp = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                temp[2-j][i] = cube[5][i][j]
 
-        cube[5] = temp
     elif c == 'F+':
         temp1 = cube[0][2][0]
         temp2 = cube[0][2][1]
@@ -249,14 +233,6 @@ def command(c):
         cube[2][2][0] = temp1
         cube[2][2][1] = temp2
         cube[2][2][2] = temp3
-
-        # 시계 회전
-        temp = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                temp[j][2-i] = cube[1][i][j]
-
-        cube[1] = temp
     elif c == 'F-':
         temp1 = cube[0][2][0]
         temp2 = cube[0][2][1]
@@ -270,21 +246,13 @@ def command(c):
         cube[2][2][1] = cube[5][0][1]
         cube[2][2][2] = cube[5][0][0]
 
-        cube[5][0][0] = cube[3][2][2]
+        cube[5][0][2] = cube[3][2][2]
         cube[5][0][1] = cube[3][2][1]
-        cube[5][0][2] = cube[3][2][0]
+        cube[5][0][0] = cube[3][2][0]
 
         cube[3][2][2] = temp3
         cube[3][2][1] = temp2
         cube[3][2][0] = temp1
-
-        # 반시계 회전
-        temp = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                temp[2-j][i] = cube[1][i][j]
-
-        cube[1] = temp
     elif c == 'B+':
         temp1 = cube[0][0][0]
         temp2 = cube[0][0][1]
@@ -306,13 +274,6 @@ def command(c):
         cube[3][0][1] = temp2
         cube[3][0][2] = temp3
 
-        # 시계 회전
-        temp = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                temp[j][2-i] = cube[4][i][j]
-
-        cube[4] = temp
     elif c == 'B-':
         temp1 = cube[0][0][0]
         temp2 = cube[0][0][1]
@@ -333,32 +294,26 @@ def command(c):
         cube[2][0][0] = temp1
         cube[2][0][1] = temp2
         cube[2][0][2] = temp3
-        # 반시계 회전
-        temp = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                temp[2-j][i] = cube[4][i][j]
-
-        cube[4] = temp
 
 
 
-    # print(c)
-    # print('                   ', cube[4][0])
-    # print('                   ', cube[4][1])
-    # print('                   ', cube[4][2])
-    # print()
-    # print(cube[3][0], '   ', cube[0][0], '   ', cube[2][0])
-    # print(cube[3][1], '   ', cube[0][1], '   ', cube[2][1])
-    # print(cube[3][2], '   ', cube[0][2], '   ', cube[2][2])
-    # print()
-    # print('                   ', cube[1][0])
-    # print('                   ', cube[1][1])
-    # print('                   ', cube[1][2])
-    # print()
-    # print('                   ', cube[5][0])
-    # print('                   ', cube[5][1])
-    # print('                   ', cube[5][2])
+
+    print(c)
+    print('                   ', cube[4][0])
+    print('                   ', cube[4][1])
+    print('                   ', cube[4][2])
+    print()
+    print(cube[3][0], '   ', cube[0][0], '   ', cube[2][0])
+    print(cube[3][1], '   ', cube[0][1], '   ', cube[2][1])
+    print(cube[3][2], '   ', cube[0][2], '   ', cube[2][2])
+    print()
+    print('                   ', cube[1][0])
+    print('                   ', cube[1][1])
+    print('                   ', cube[1][2])
+    print()
+    print('                   ', cube[5][0])
+    print('                   ', cube[5][1])
+    print('                   ', cube[5][2])
 
 
 for _ in range(n):
@@ -376,4 +331,5 @@ for _ in range(n):
         command(arr[i])
 
     for i in range(3):
-        print(''.join(cube[0][i]))
+        print(cube[0][i])
+    print()
